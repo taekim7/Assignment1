1875f54628c4e37fb96dfa334c748f759ab01fbe
Tae Kim <taekim7@hawaii.edu>
2023-11-08 16:33:47 -1000

    23

--- AssignmentCopy2/server.js
+++ AssignmentCopy2/server.js
@@ -2,13 +2,9 @@
 
 // Importing the Express.js framework 
 const express = require('express');
-let fs = require('fs');
-let path = require ('path');
-
 // Create an instance of the Express application called "app"
 // app will be used to define routes, handle requests, etc
 const app = express();
-let port = 8080;
 
 // Monitor all requests regardless of their method (GET, POST, PUT, etc) and their path (URL)
 app.all('*', function (request, response, next) {
@@ -16,29 +12,21 @@ app.all('*', function (request, response, next) {
    next();
 });
 
-app.get("/products.js", function (request, response, next) {
-   response.type('.js');
-   let products_str = `let products = ${JSON.stringify(products)};`;
-   response.send(products_str);
-});
-
-
 // Route all other GET requests to serve static files from a directory named "public"
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
+let products = require(__dirname + "/products.json");
 
-// Create a route to serve product data from 'products.json'
-app.get('/api/products', (req, res) => {
-   fs.readFile('products.json', 'utf8', (err, data) => {
-     if (err) {
-       console.error(err);
-       res.status(500).json({ error: 'Internal Server Error' });
-       return;
-     }
-     const products = JSON.parse(data);
-     res.json(products);
-   });
- });
-
+// Define a route for handling a GET request to a path that matches "./products.js"
+app.get('./products.js', function(request, response, next) {
+	// Send the response as JS
+	response.type('.js');
+	
+	// Create a JS string (products_str) that contains data loaded from the products.json file
+	// Convert the JS string into a JSON string and embed it within variable products
+	const products_str = `let products = ${JSON.stringify(products)};`;
+	
+	// Send the string in response to the GET request
+	response.send(products_str);
+});
 // Start the server; listen on port 8080 for incoming HTTP requests
-app.listen(8080, () => { console.log(`listening on ${port}`); }
-);
+app.listen(8080, () => console.log(`listening on port 8080`));
