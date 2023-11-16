ec0f03076732d334b68cb13e13c07aadb029d5e7
Tae Kim <taekim7@hawaii.edu>
2023-11-15 22:13:15 -1000

    23

--- public/invoice.js
+++ public/invoice.js
@@ -1,99 +1,113 @@
-//invoice.js
+//Invoice JS
 
 
-//Product Data
-const params = (new URL (document.location)).searchParams;
-let quantity = [];
-let q = Number(params.get('quantity'));
-let error = params.get('error');
-
-if (error){
-  alert(error);
-}
-
-for (let i = 0; i < products.length; i++) {
-  let quantityValue = params.get(`quantity${i}`);
-  if (quantityValue !== null){
-    quantity [products[i].quantityIndex] = Number (quantityValue);
-  }
-}
-
-
-
-//Variables for subtotal, tax, shipping charge, and total
+//initializes variables
+let extendedPrices = [];
+let extendedPrice = 0;
 let subtotal = 0;
-let taxRate = 0.04;
 let taxAmount = 0;
-let total = 0;
-let shippingCharge = 0;
+let shipping = 0;
 
+//opens the url params
+let params = (new URL(document.location)).searchParams;
+        //initializes empty order array
+        let order = [];
+        //for each prod, push the value to the array
+        params.forEach((value,key) => {
+            if (key.startsWith('prod')) {
+                    order.push(parseInt(value));
+                }
+        });
+
+        
+//generate all the item rows
 generateItemRows();
 
-if (subtotal <= 300) {
-  shippingCharge = 5;
-} else if (subtotal <= 600) {
-  shippingCharge = 10;
-} else {
-  shippingCharge = subtotal * 0.04;
+//calculate tax
+ let tax = (subtotal*0.04);
+
+//checks the shipping price
+if(subtotal <= 300)
+{
+    shipping = 20;
+}else if(subtotal <=600)
+{
+    shipping = 40;
+}
+else{
+    shipping = subtotal*.04;
 }
 
-//Calculate total with shipping
-taxAmount = subtotal * taxRate;
-total = subtotal + taxAmount + shippingCharge;
+//calculates total
+let total = tax+subtotal+shipping;
 
 
-//Setting subtotal, tax, and total cells
-document.getElementById('subtotal_cell').innerHTML = '$' + subtotal.toFixed(2);
-document.getElementById('tax_cell').innerHTML = '$' + taxAmount.toFixed(2);
-document.getElementById('shipping_cell').innerHTML = '$' + shippingCharge.toFixed(2);
-document.getElementById('total_cell').innerHTML = `$${total.toFixed(2)}`;
+//insert footer row values
+document.getElementById("subtotal_cell").innerHTML = "$" + subtotal.toFixed(2);
+document.getElementById("tax_cell").innerHTML = "$" + tax.toFixed(2);
+document.getElementById("shipping_cell").innerHTML = "$"+shipping.toFixed(2);
+document.getElementById("total_cell").innerHTML = "$"+total.toFixed(2);
 
-//I tried a different method..but it didn't work
-function validateQuantity (quantity) {
-  if (isNaN(quantity)) {
-  return "Not a number";
-  }else if (quantity < 0 && !Number.isInteger(quantity)) {
-  return "Negative Inventory and not an integer";
-  }else if (quantity <0){
-  return "Negative Inventory";
-  }else if (!Number.isInteger(quantity)){
-  return "not an integer";
-  }else {
-  return "";
-  }
-  }
-  
 
-//function to generate table rows and apply quantity validation
-function generateItemRows() {
-  let table = document.getElementById('invoiceTable');
-  table.innerHTML = '';
-  let hasErrors = false;
-  
-  for (let i = 0; i < products.length; i++) {
-    let item = products[i];
-    let itemQuantity = quantity[item.quantityIndex];
-
-    let validationMessage = validateQuantity(itemQuantity);
-    if (validationMessage !== "") {
-      hasErrors = true;
-      let row = table.insertRow();
-      row.insertCell(1).innerHTML = item.name;
-      row.insertCell(2).innerHTML = validationMessage;
-    } else if (itemQuantity > 0) {
-      let extendedPrice = item.price * itemQuantity;
-      subtotal += extendedPrice;
-
-      let row = table.insertRow();
-      row.insertCell(0).innerHTML = `<img src="${item.image}" class="table-image" alt="Product Image">`;
-      row.insertCell(1).innerHTML = item.name;
-      row.insertCell(2).innerHTML = itemQuantity;
-      row.insertCell(3).innerHTML = '$' + item.price.toFixed(2);
-      row.insertCell(4).innerHTML = '$' + extendedPrice.toFixed(2);
+//function to validate the quantity, returns a string if not a number, negative, not an integer, or a combination of both
+//if no errors in quantity, returns empty string
+function validateQuantity(quantity){
+    if(isNaN(quantity)){
+        return "Please Enter a Number";
+    }else if (quantity<0 && !Number.isInteger(quantity)){
+        return "Please Enter a Positive Integer";
+    }else if (quantity <0){
+        return "Please Enter a Positive Number";
+    }else if(!Number.isInteger(quantity)){
+        return "Please Enter an Integer";
+    }else{
+        return"";
     }
+
+}
+//generate all the item rows
+function generateItemRows(){
+
+    //sets table to the invoice table on the html
+    let table = document.getElementById("invoiceTable");
+
+    //checks if it has errors, set it to no for now
+    let hasErrors = false; 
+
+    //for each member of the array
+    for(let i=0;i<products.length;i++){
+        
+        //sets item and itemQuantity from the products array, and the array gotten from the url
+        let item = products[i];
+        let itemQuantity = order[i];
+        
+        //validate the quantity, we are just kinda looking for if its negative so we dont show it
+        let validationMessage = validateQuantity(itemQuantity);
+        
+        
+        //if there is an error, just ignore this 
+        if(validationMessage !== ""){
+            hasErrors = true;
+            let row =table.insertRow();
+            row.insertCell(0).insertHTML = item.name;
+            row.insertCell(1).innerHTML = validationMessage;
+        } 
+        //otherwise, lets create the row in the invoice and update the extended price and subtotal
+        else if(itemQuantity >0){
+            //update the variables
+            extendedPrice = item.price * itemQuantity;
+            subtotal += extendedPrice;
+
+            //create a new row and insert the info
+            let row = table.insertRow();
+            row.insertCell(0).innerHTML = `<img src="${item.image}" class="img-small" name = "img" data-tooltip="${item.description}">`;
+            row.insertCell(1).innerHTML = item.name;
+            row.insertCell(2).innerHTML = itemQuantity;
+            row.insertCell(3).innerHTML = "$" + item.price.toFixed(2);
+            row.insertCell(4).innerHTML = "$"+extendedPrice.toFixed(2);
+
+        }
+
     }
-    //if no error, display total
-    if (!hasErrors){
-      document.getElementById('total_cell').innerHTML = '$' + total.toFixed(2);
-    }
-  }
+
+}
--- public/products_display.js
+++ public/products_display.js
@@ -1,140 +1,78 @@
 //products_display.js
 
-const productForm = document.getElementById('productForm');
-
-
-// declare and push to the DOM the store name at top and bottom
-let store_name = "Cool Shoe Store";
-top_title.innerHTML = store_name;
-// send store name information to the footer title
-bottom_title.innerHTML = "Wear History. It's cool.";
-
-let productDisplayContainer = document.getElementById('product-display-container');
-
-// Function to generate table rows and apply quantity validation
-for (let i = 0; i < products.length; i++) {
-  const productDiv = document.createElement('div');
-  productDiv.classList.add('product-item'); // Add a class to the product item
-
-  productDiv.innerHTML = `
-        <h2>${products[i]["name"]}  \$${products[i]["price"]}</h2>
-        <p style="color: black;">Quantity: <span class="quantity">${products[i]["qty_available"]}</span></p>
-        <img src="${products[i]["image"]}" style="width:350px; height: auto;"/>
-        <input type="text" class="quantity-input" name="quantity_textbox${i}" value="0" min="0" data-max="${products[i]["qty_available"]}"/>
-        <span class="quantity-message" id="quantity_textbox_${i}_message">Enter a quantity</span>
-    `;
-  productDisplayContainer.appendChild(productDiv);
-  };
-
-
-
-
-/*
-  //Declare params
+//set up params from headder, order array, and error value
 let params = (new URL(document.location)).searchParams;
-if (params.has('error')) {
-    document.getElementById('error-message').innerHTML = "no quantities selected";
-    setTimeout (() => {
-        document.getElementById('error-message').innerHTML = "";
-    }, 3000);
-    }
-    for (let i in products) {
-        if (params.get(`qty${i}`) == 0) {
-            qty_form[`qty${i}_entered`].value = '';
-        }else{
-            qty_form[`qty${i}_entered`].value = params.get(`qty${i}`);
-            qty_form[`qty${i}_entered`].parentElement.style.borderColor = "red";
+let error;
+let order = [];
+
+//get if there was an error before
+error = params.get('error');
+
+//fill order array with item ammounts from previous attempts
+params.forEach((value,key) => {
+    if (key.startsWith('prod')) {
+            order.push(parseInt(value));
         }
-        errors = isNonNegInt(params.get(`qty${i}`), true)
-        document.getElementById(`qty${i}_error`).innerHTML = errors.join('');
-            
-    }
-*/
-
-// Get the URL
-let params = (new URL(document.location)).searchParams;
-
-window.onload = function() {
-    /* If there is a server side validation error
-    Display message to user and allow them to edit their inputs
-    User input is made sticky by retrieving quantities from the URL 
-    Those inputs are validated by isNonNegInt again */
-
-    if (params.has('error')) {
-       
-        document.getElementById('errMsg').innerHTML = "No quantities selected.";
-        setTimeout(() => {
-            document.getElementById('errMsg').innerHTML = "";
-        }, 2000);
-    } 
-    else if (params.has('inputErr')) {
-        document.getElementById('errMsg').innerHTML = "Please fix errors before proceeding."
-        setTimeout(() => {
-            document.getElementById('errMsg').innerHTML = "";
-        }, 2000);
-
-        for (let i in products) {
-            if (params.get(`qty${i}`) == 0) {
-                qty_form[`qty${i}_entered`].value = '';
-            } else {
-                qty_form[`qty${i}_entered`].value = params.get(`qty${i}`);
-                qty_form[`qty${i}_entered`].parentElement.style.borderColor = "red";
-            }
-            errors = validateQuantity(params.get(`qty${i}`))
-            document.getElementById(`qty${i}_error`).innerHTML = errors.join('');  
-        }
-        alert("errors = "+errors);
-    }
-}
-
-
-
-
-// Add an event listener to each quantity input for real-time validation
-document.querySelectorAll('.quantity-input').forEach((input, i) => {
-  input.addEventListener('input', function () {
-      const quantityMessage = document.getElementById(`quantity_textbox_${i}_message`);
-      const qty = Number(this.value);
-      const validationMessage = validateQuantity(qty, products[i]["qty_available"]);
-
-      // Update error message dynamically
-      quantityMessage.textContent = validationMessage;
-  });
 });
+console.log('Order Array:', order);
 
-
-  
-// Function to validate quantity
-function validateQuantity(quantity, maxQuantity) {
-  let errorMessage = "";
-
-  switch (true) {
-      case isNaN(quantity):
-          errorMessage = "Not a number. Please enter a non-negative quantity to order.";
-          break;
-      case quantity <= 0 && !Number.isInteger(quantity):
-          errorMessage = "Negative inventory and not an Integer. Please enter a non-negative quantity to order.";
-          break;
-      case quantity <= 0:
-          errorMessage = "Negative inventory. Please enter a non-negative quantity to order.";
-          break;
-      case !Number.isInteger(quantity):
-          errorMessage = "Not an Integer. Please enter a non-negative quantity to order.";
-          break;
-      case quantity > maxQuantity:
-          errorMessage = `Quantity exceeds the available stock (${maxQuantity}). Please enter a valid quantity.`;
-          break;
-      default:
-          errorMessage = ""; // No errors
-          break;
-  }
-
-  return errorMessage;
+//if there is an error submitted, then show the error text in errorDiv
+if(error == 'true'){
+    
+    document.getElementById('errorDiv').innerHTML += `<h2 class="text-danger"></h2><br>`;
 }
 
+//Populate products display with product information
 
-//autoplay music
-function playAudio() {
-  var audio = document.getElementById("background-music");
-  audio.play();
-}
+for (let i = 0; i < products.length; i++) {
+    document.querySelector('.row').innerHTML += 
+        `<div class="col-md-6 product_name mb-4">
+        <div class="name">
+            <div class="text-center">
+                <img src="${products[i].image}" class="name-img" alt="Product Image" data-tooltip="${products[i].description}">
+            </div>
+            <div class="name-body">
+                <h5 class="name-title">${products[i].name}</h5>
+                <p class="name-text">
+                    Price: $${(products[i].price).toFixed(2)}<br>
+                    Available: ${products[i].qty_available}<br>
+                    Total Sold: ${products[i].total_sold}
+                </p>
+                
+                <input type="text" placeholder="0" name="quantity_textbox" id="${[i]}" class="form-control" oninput="validateQuantity(this)" value="${order[i] !== 0 && order[i] !== undefined ? order[i] : ''}" onload="validateQuantity(this)">
+                <p id="invalidQuantity${[i]}" class="text-danger"></p>
+                </div>
+            </div>
+        </div>`
+        validateQuantity(document.getElementById(`${[i]}`));
+ ;}
+
+//Validate each quantity and throw out error message if something is wrong. 
+    function validateQuantity(quantity){
+        //set variables, and grab number from the quantity and set it to an number
+        let valMessage = '';
+        let quantityNumber = Number(quantity.value);
+        //console.log(Number.isInteger(quantityNumber));
+        document.getElementById(`invalidQuantity${quantity.id}`).innerHTML = "validationMessage";
+        //console.log(products[quantity.id]['qty_available']);
+        //gets validation message if not a number, negative, not an integer, or if there is not enough items in stock
+        //else  empty string 
+        if(isNaN(quantityNumber)){
+            valMessage = "Please Enter a Number";
+        }else if (quantityNumber<0 && !Number.isInteger(quantityNumber)){
+            valMessage = "Please Enter a Positive Integer";
+        }else if (quantityNumber <0){
+            valMessage = "Please Enter a Positive Value";
+        }else if(!Number.isInteger(quantityNumber)){
+            valMessage = "Please Enter an Integer";
+        }else if(quantityNumber > products[quantity.id]['qty_available']){
+            valMessage = "Not Enough Items in Stock!";
+        }
+        else{
+            valMessage = '';
+        }
+        //set the valMessage to the innerHTML to the section
+        document.getElementById(`invalidQuantity${quantity.id}`).innerHTML = valMessage;
+        //console.log(products[quantity.id])
+    }
+
